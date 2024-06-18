8d336ba2d0c2bb30eece3c58d0fca33b3c3ae61c
lucas-martins <lucasmartinsnara@gmail.com>
2024-06-18 10:13:42 -0300

    corrigindo a conexao com o database

--- api/src/banco/banco.go
+++ api/src/banco/banco.go
@@ -9,8 +9,8 @@ import (
 )
 
 func Connction() (*sql.DB, error) {
-	stringConection := fmt.Sprintf("%s:%s@/%s?charset=ut8&parseTime=True&loc=Local",
-		os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_NAME"))
+	stringConection := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
+		os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
 	db, err := sql.Open("mysql", stringConection)
 	if err != nil {
 		return nil, err

b162d61eefaf6121bfba33d8a392d1c8c9c4552d
lucas <lucasmartinsnara@gmail.com>
2024-07-01 10:54:47 -0300

    criando pagina de login

--- webapp/src/router/rotas/rotas.go
+++ webapp/src/router/rotas/rotas.go
@@ -20,5 +20,10 @@ func Configurar(router *mux.Router) *mux.Router {
 	for _, rota := range rotas {
 		router.HandleFunc(rota.URI, rota.Funcao).Methods(rota.Method)
 	}
+
+	// Esta estrutura vai possibilitar que o Go consiga passar para o Navegador os Styles CSS
+	fileServer := http.FileServer(http.Dir("./assets/"))
+	router.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", fileServer))
+
 	return router
 }

f1d6685da2562325f67e5603bc4a9d951469787a
lucas <lucasmartinsnara@gmail.com>
2024-07-02 10:15:20 -0300

    rota de criacao de usauraio funcionando

--- webapp/src/controller/usuario.go
+++ webapp/src/controller/usuario.go
@@ -1,17 +1,22 @@
 package controller
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+// Base URL
+var url = "http://localhost:5000/"
+
 // Vai chamar a API para a criaçao de um novo Cadastro de usuario
 func CadastroOfUser(w http.ResponseWriter, r *http.Request) {
 	// Ele vai pegar o Bode da request e vai deixar os dados acessiveis para manipulação
 	r.ParseForm()
 
+	// Capturando os dados do Body e transformando em um array de Bytes
 	usuario, err := json.Marshal(map[string]string{
 		"nome":  r.FormValue("nome"),
 		"nick":  r.FormValue("nick"),
@@ -21,6 +26,10 @@ func CadastroOfUser(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Fatal(err)
 	}
+	response, err := http.Post(fmt.Sprintf("%susuario", url), "application/json", bytes.NewBuffer(usuario))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	fmt.Println(usuario)
+	defer response.Body.Close()
 }
